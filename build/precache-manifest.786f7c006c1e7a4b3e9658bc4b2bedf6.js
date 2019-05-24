66286c4a3dada550f0e40ef08d8b9f1ef9d7fb7c
msesno <msesno@gmail.com>
2019-05-24 14:07:55 -0400

    add working files

---
+++ build/precache-manifest.786f7c006c1e7a4b3e9658bc4b2bedf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2f01e55af11efd9683481baa695f9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a8f5e41d7461e5462aa",
+    "url": "/static/css/main.97f1e7cb.chunk.css"
+  },
+  {
+    "revision": "57b9249c39c5d9fbaf45",
+    "url": "/static/js/2.a0c84e1e.chunk.js"
+  },
+  {
+    "revision": "6a8f5e41d7461e5462aa",
+    "url": "/static/js/main.6e90dc4d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
